88eea3085b12485d2cf87ddd2c372d43da566165
Rifki Ramadhan <riifkiramadhan2@gmail.com>
2022-06-13 12:31:07 +0700

    Mengirim perubahan script pada components

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,16 +1,13 @@
-import React, { Component } from 'react';
-import './Footer.css';
+import React from 'react';
 
-class Footer extends Component {
-  render() {
-    return (
-      <div className="row footer bg-light">
-        <div className="col text-center">
-          <p>2022 All Right Reseverd by Rifki Ramadhan.</p>
-        </div>
+function Footer() {
+  return (
+    <div className="row footer bg-light">
+      <div className="col text-center">
+        <p>2022 All Right Reseverd by Rifki Ramadhan.</p>
       </div>
-    );
-  }
+    </div>
+  );
 }
 
 export default Footer;
